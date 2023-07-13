e9f570556119dea9175e2ab13d1e87bc4fbd5fd6
DiegoGallardo <diego.gallardo@beatcode.it>
2023-07-13 12:35:37 +0200

    chore: removed commented code

--- CoreMLBackgroundChangeSwiftUI/CoreMLBackgroundChangeSwiftUI/ContentView.swift
+++ CoreMLBackgroundChangeSwiftUI/CoreMLBackgroundChangeSwiftUI/ContentView.swift
@@ -65,12 +65,6 @@ struct ContentView: View {
     }
     
     func maskInputImage(){
-        
-//        let points = [GradientPoint(location: 0, color: #colorLiteral(red: 0.6486759186, green: 0.2260715365, blue: 0.2819285393, alpha: 1)), GradientPoint(location: 0.2, color: #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 0.5028884243)), GradientPoint(location: 0.4, color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 0.3388534331)),
-//                  GradientPoint(location: 0.6, color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 0.3458681778)), GradientPoint(location: 0.8, color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 0.3388534331))]
-//
-//        let bgImage = UIImage(size: self.inputImage.size, gradientPoints: points, scale: self.inputImage.scale)!
-        
         let bgImage = UIImage.imageFromColor(color: .orange, size: self.inputImage.size, scale: self.inputImage.scale)!
 
         let beginImage = CIImage(cgImage: inputImage.cgImage!)
